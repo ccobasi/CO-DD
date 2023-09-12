f4e1d25325b9745e45abe19dc7d29b9da0620b13
ccobasi <ccobasi8@gmail.com>
2023-09-12 13:40:22 +0100

    Add progress loader on success page

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -42,9 +42,15 @@
       <img src="../assets/draw.jpg" alt="success">
       <h2>Great Job!</h2>
       <p>You have successfully created an event. Sit tight while it finishes loading</p>
-      <p>Loading....</p>
-      <v-progress-linear v-model="value" :buffer-value="bufferValue" color="" class="bar"></v-progress-linear>
-      <p>Redirecting in {{ countdown }} seconds...</p>
+      <!-- <p>Loading....</p> -->
+      <!-- <div class="loader-wrapper">
+        <svg>
+          <circle cx="70" cy="70" r="50"></circle>
+        </svg>
+      </div> -->
+
+      <p style="visibility:hidden">Redirecting in {{ countdown }} seconds...</p>
+      <div class="loader"></div>
     </div>
   </body>
 </template>
@@ -81,7 +87,136 @@ body {
   font-weight: 400;
   line-height: 19.2px;
 }
-bar {
-  background: var(--gradient, linear-gradient(90deg, #227cbf 0%, #47b65c 100%));
+.loader-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.loader-wrapper svg {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  animation: rotate 2s linear infinite;
+}
+.loader-wrapper svg circle {
+  width: 100%;
+  height: 100%;
+  fill: none;
+  stroke-width: 10;
+  stroke: #59b976, #2c84b9;
+  stroke-linecap: round;
+  transform: translate(5px, 5px);
+  stroke-dasharray: 440;
+  stroke-dashoffset: 440;
+  animation: animate 4s linear infinite;
+}
+@keyframes animate {
+  0%,
+  100% {
+    stroke-dashoffset: 440;
+  }
+  50% {
+    stroke-dashoffset: 0;
+  }
+  50.1% {
+    stroke-dashoffset: 880;
+  }
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+/*Next*/
+.loader,
+.loader:before,
+.loader:after {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  content: '';
+  position: absolute;
+  border-radius: 50%;
+}
+
+.loader {
+  position: fixed;
+  width: 100px;
+  height: 100px;
+  margin-top: 450px;
+  margin-left: 50%;
+  animation: spin 4s linear infinite;
+}
+
+@keyframes spin {
+  100% {
+    transform: rotate(360deg);
+    filter: hue-rotate(360deg);
+  }
+}
+
+.loader:before {
+  border: 5px solid #2c84b9;
+  border-bottom: 5px solid #59b976;
+  border-left: 5px solid #59b976;
+  animation: spin1 1s linear infinite;
+}
+
+.loader:after {
+  border: 5px solid #2c84b9;
+  border-top: 5px solid transparent;
+  border-right: 5px solid transparent;
+  animation: spin2 1s linear infinite;
+}
+
+@keyframes spin1 {
+  20% {
+    transform: rotate(150deg);
+  }
+  40% {
+    transform: rotate(300deg);
+  }
+  80% {
+    transform: rotate(300deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes spin2 {
+  0% {
+    transform: rotate(-30deg);
+  }
+  20% {
+    transform: rotate(-30deg);
+    border-color: transparent transparent #2c84b9 #2c84b9;
+  }
+  21% {
+    border-color: #59b976 #59b976 transparent transparent;
+  }
+  40% {
+    transform: rotate(-30deg);
+  }
+  60% {
+    transform: rotate(120deg);
+    border-color: #59b976 #59b976 transparent transparent;
+  }
+  61% {
+    border-color: transparent transparent #2c84b9 #2c84b9;
+  }
+  80% {
+    transform: rotate(270deg);
+  }
+  100% {
+    transform: rotate(330deg);
+  }
 }
 </style>
